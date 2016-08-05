9a1232cc10647a99e6284634013042746702852c
Venkatakrishnan Shankar <venkatakrishnan.shankar@monsanto.com>
2016-08-05 15:46:17 -0500

    lesson 8, changed react router version

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,4 +1,5 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import { toggleTodo } from '../actions';
 import TodoList from './TodoList';
 
@@ -15,9 +16,9 @@ const getVisibleTodos = (todos, filter) => {
   }
 };
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state, { params }) => {
   return {
-    todos: getVisibleTodos(state.todos, ownProps.filter)
+    todos: getVisibleTodos(state.todos, params.filter || 'all')
   };
 };
 
@@ -29,9 +30,9 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-const VisibleTodoList = connect(
+const VisibleTodoList = withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
-)(TodoList);
+)(TodoList));
 
 export default VisibleTodoList;
